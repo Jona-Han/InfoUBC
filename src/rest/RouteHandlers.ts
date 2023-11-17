4cc2cefad98a488d621aaf708915642e0167341e
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-11-17 12:56:31 -0800

    Fix linting

--- src/rest/RouteHandlers.ts
+++ src/rest/RouteHandlers.ts
@@ -1,29 +1,29 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import InsightFacade from "../controller/InsightFacade";
-import { InsightError, NotFoundError } from "../controller/IInsightFacade";
+import {InsightError, NotFoundError} from "../controller/IInsightFacade";
 
 export default class RouteHandlers {
-    public static async getAllDatasets(req: Request, res: Response) {
+	public static async getAllDatasets(req: Request, res: Response) {
 		try {
-            const fc: InsightFacade = new InsightFacade();
-            const results = await fc.listDatasets();
-            res.status(200).json({result: results});
-		} catch (err: unknown) {
-            res.status(404).json({error: err});
+			const fc: InsightFacade = new InsightFacade();
+			const results = await fc.listDatasets();
+			res.status(200).json({result: results});
+		} catch (err) {
+			res.status(404).json({error: err});
 		}
 	}
 
-    public static async deleteDataset(req: Request, res: Response) {
-        try {
-            const fc: InsightFacade = new InsightFacade();
-            const results = await fc.removeDataset(req.params.id);
-            res.status(200).json({result: results});
-        } catch (err: any) {
-            if (err instanceof NotFoundError) {
-                res.status(404).json({error: err});
-            } else {
-                res.status(400).json({error: err});      
-            }
-        }
-    }
+	public static async deleteDataset(req: Request, res: Response) {
+		try {
+			const fc: InsightFacade = new InsightFacade();
+			const results = await fc.removeDataset(req.params.id);
+			res.status(200).json({result: results});
+		} catch (err) {
+			if (err instanceof NotFoundError) {
+				res.status(404).json({error: err});
+			} else {
+				res.status(400).json({error: err});
+			}
+		}
+	}
 }
