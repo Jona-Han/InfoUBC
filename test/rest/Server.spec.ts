d52528225952aa5126c3a07fc1133b7b6db9cd03
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-11-21 04:04:07 -0800

    Add 2 tests for put endpoint

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -3,51 +3,82 @@ import InsightFacade from "../../src/controller/InsightFacade";
 
 import {expect} from "chai";
 import request, {Response} from "supertest";
+import fs from "fs-extra";
+import { clearDisk } from "../TestUtil";
 
 describe("Facade D3", function () {
 
 	let facade: InsightFacade;
 	let server: Server;
 
-	before(function () {
+	const SERVER_URL = "http://localhost:4321";
+
+	before(async function () {
 		facade = new InsightFacade();
 		server = new Server(4321);
 		// TODO: start server here once and handle errors properly
+		try {
+			await server.start();
+			console.log("Server started successfully");
+		} catch (err) {
+			console.error("Failed to start server:", err);
+		}
 	});
 
-	after(function () {
+	after(async function () {
 		// TODO: stop server here once!
+		try {
+			await server.stop();
+			console.log("Server stopped successfully");
+		} catch (err) {
+			console.error("Failed to stop server:", err);
+		}
 	});
 
 	beforeEach(function () {
 		// might want to add some process logging here to keep track of what is going on
+		console.log("Starting a new test case.");
 	});
 
 	afterEach(function () {
 		// might want to add some process logging here to keep track of what is going on
+        clearDisk();
 	});
 
-	// Sample on how to format PUT requests
-	/*
-	it("PUT test for courses dataset", function () {
+	it.only("PUT test for courses dataset", async function () {
 		try {
-			return request(SERVER_URL)
-				.put(ENDPOINT_URL)
-				.send(ZIP_FILE_DATA)
-				.set("Content-Type", "application/x-zip-compressed")
-				.then(function (res: Response) {
-					// some logging here please!
-					expect(res.status).to.be.equal(200);
-				})
-				.catch(function (err) {
-					// some logging here please!
-					expect.fail();
-				});
+			const data = await fs.readFile("test/resources/archives/pair.zip");
+
+			const res = await request(SERVER_URL)
+				.put("/dataset/ubc/sections")
+				.send(data)
+				.set("Content-Type", "application/x-zip-compressed");
+
+			expect(res.status).to.equal(200);
+			expect(res.body).to.have.property("result");
+            expect(res.body.result).to.deep.equal(['ubc']);
 		} catch (err) {
-			// and some more logging here!
+            expect.fail()
 		}
 	});
-	*/
+
+
+	it.only("PUT test for rooms dataset", async function () {
+		try {
+			const data = await fs.readFile("test/resources/archives/campus.zip");
+
+			const res = await request(SERVER_URL)
+				.put("/dataset/ubc/rooms")
+				.send(data)
+				.set("Content-Type", "application/x-zip-compressed");
+
+			expect(res.status).to.equal(200);
+			expect(res.body).to.have.property("result");
+            expect(res.body.result).to.deep.equal(['ubc']);
+		} catch (err) {
+            expect.fail()
+		}
+	});
 
 	// The other endpoints work similarly. You should be able to find all instructions at the supertest documentation
 });
