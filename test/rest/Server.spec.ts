47bc8f0ca8095b890f96f3998bd94d25a032f502
Jonathan Han <87393036+Jona-Han@users.noreply.github.com>
2023-11-21 13:09:12 -0800

    Finish tests for backend

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -2,16 +2,19 @@ import Server from "../../src/rest/Server";
 import InsightFacade from "../../src/controller/InsightFacade";
 
 import {expect} from "chai";
-import request, {Response} from "supertest";
+import request from "supertest";
 import fs from "fs-extra";
 import {clearDisk} from "../TestUtil";
 
 describe("Facade D3", function () {
-
 	let facade: InsightFacade;
 	let server: Server;
 
 	const SERVER_URL = "http://localhost:4321";
+	const sectionsData = fs.readFileSync("test/resources/archives/pair.zip");
+	const smallSectionsData = fs.readFileSync("test/resources/archives/only1section.zip");
+	const roomsData = fs.readFileSync("test/resources/archives/campus.zip");
+	const smallRoomsData = fs.readFileSync("test/resources/archives/1validBuilding.zip");
 
 	before(async function () {
 		facade = new InsightFacade();
@@ -19,9 +22,9 @@ describe("Facade D3", function () {
 		// TODO: start server here once and handle errors properly
 		try {
 			await server.start();
-			console.log("Server started successfully");
+			console.log("Server::Server started successfully");
 		} catch (err) {
-			console.error("Failed to start server:", err);
+			console.error("Server::Failed to start server:", err);
 		}
 	});
 
@@ -29,191 +32,456 @@ describe("Facade D3", function () {
 		// TODO: stop server here once!
 		try {
 			await server.stop();
-			console.log("Server stopped successfully");
+			console.log("Server::Server stopped successfully");
 		} catch (err) {
-			console.error("Failed to stop server:", err);
+			console.error("Server::Failed to stop server:", err);
 		}
 	});
 
 	beforeEach(function () {
 		// might want to add some process logging here to keep track of what is going on
-		console.log("Starting a new test case.");
-	});
-
-	afterEach(function () {
-		// might want to add some process logging here to keep track of what is going on
-		clearDisk();
 	});
 
 	describe("PUT tests", function () {
-		it.only("PUT test for courses dataset", async function () {
-			try {
-				const data = await fs.readFile("test/resources/archives/pair.zip");
-
-				const res = await request(SERVER_URL)
-					.put("/dataset/ubc/sections")
-					.send(data)
-					.set("Content-Type", "application/x-zip-compressed");
-
-				expect(res.status).to.equal(200);
-				expect(res.body).to.have.property("result");
-				expect(res.body.result).to.deep.equal(["ubc"]);
-			} catch (err) {
-				expect.fail();
-			}
+		afterEach(function () {
+			// might want to add some process logging here to keep track of what is going on
+			clearDisk();
 		});
 
-		it.only("PUT test for rooms dataset", async function () {
+		it("should add valid rooms dataset", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/campus.zip");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/rooms")
-					.send(data)
+					.send(roomsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(200);
 				expect(res.body).to.have.property("result");
 				expect(res.body.result).to.deep.equal(["ubc"]);
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT test invalid kind", async function () {
+		it("should throw error for invalid kind", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/campus.zip");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/invalid")
-					.send(data)
+					.send(sectionsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - multiple adds in series", async function () {
+		it("should successfully add multiple datasets in series", async function () {
 			try {
-				let data = await fs.readFile("test/resources/archives/pair.zip");
-
 				await request(SERVER_URL)
 					.put("/dataset/ubc/sections")
-					.send(data)
+					.send(sectionsData)
 					.set("Content-Type", "application/x-zip-compressed");
-
-				data = await fs.readFile("test/resources/archives/campus.zip");
 
 				const res = await request(SERVER_URL)
 					.put("/dataset/second/rooms")
-					.send(data)
+					.send(roomsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(200);
 				expect(res.body).to.have.property("result");
 				expect(res.body.result).to.eql(["ubc", "second"]);
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - adding an sections formatted zip as rooms kind", async function () {
+		it("should have error for rooms kind with sections dataset", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/pair.zip");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/rooms")
-					.send(data)
+					.send(sectionsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - adding an rooms formatted zip as sections kind", async function () {
+		it("should have error for invalid key", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/1ValidBuilding.zip");
-
 				const res = await request(SERVER_URL)
-					.put("/dataset/ubc/sections")
-					.send(data)
+					.put("/dataset/ubc_j/sections")
+					.send(roomsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - invalid zip", async function () {
+		it("should have error for sections kind with rooms dataset", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/noValidSections.zip");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/sections")
-					.send(data)
+					.send(roomsData)
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - txt file not zip", async function () {
+		it("should have error for invalid dataset", async function () {
 			try {
-				const data = await fs.readFile("test/resources/archives/notAZip.txt");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/sections")
-					.send(data)
+					.send(fs.readFileSync("test/resources/archives/noValidSections.zip"))
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 
-		it.only("PUT - already base 64", async function () {
+		it("should have an error for not a zip", async function () {
 			try {
-				const data = (await fs.readFile("test/resources/archives/only1section.zip")).toString("base64");
-
 				const res = await request(SERVER_URL)
 					.put("/dataset/ubc/sections")
-					.send(data)
+					.send(fs.readFileSync("test/resources/archives/notAZip.txt"))
 					.set("Content-Type", "application/x-zip-compressed");
 
 				expect(res.status).to.equal(400);
 				expect(res.body).to.have.property("err");
-			} catch (err) {
-				expect.fail();
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should have an error for already in base 64", async function () {
+			try {
+				const res = await request(SERVER_URL)
+					.put("/dataset/ubc/sections")
+					.send(sectionsData.toString("base64"))
+					.set("Content-Type", "application/x-zip-compressed");
+
+				expect(res.status).to.equal(400);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
 			}
 		});
 	});
-
 
 	describe("GET", function () {
-		it("should return an empty list of datasets", async function() {
-			const res = await request(server)
-				.get("/datasets");
+		afterEach(function () {
+			// might want to add some process logging here to keep track of what is going on
+			clearDisk();
+		});
 
-			expect(res.status).to.equal(200);
-			expect(res.body).to.have.property("result");
-			expect(res.body.result).to.be.an("array");
-			expect(res.body.result).to.equal([]);
+		it("should return an empty list of datasets", async function () {
+			try {
+				const res = await request(SERVER_URL).get("/datasets");
+
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("result");
+				expect(res.body.result).to.be.an("array");
+				expect(res.body.result).to.eql([]);
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should return one dataset", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).get("/datasets");
+
+				expect(res.status).to.equal(200);
+				expect(res.body.result).to.have.lengthOf(1);
+				expect(res.body.result).to.eql([{id: "ubc", kind: "rooms", numRows: 4}]);
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should return 2 datasets", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				await request(SERVER_URL)
+					.put("/dataset/second/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).get("/datasets");
+
+				expect(res.status).to.equal(200);
+				expect(res.body.result).to.have.lengthOf(2);
+				expect(res.body.result).to.have.deep.members([
+					{id: "ubc", kind: "rooms", numRows: 4},
+					{id: "second", kind: "rooms", numRows: 4},
+				]);
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
 		});
 	});
 
+	describe("DELETE", function () {
+		afterEach(function () {
+			// might want to add some process logging here to keep track of what is going on
+			clearDisk();
+		});
 
+		it("should delete rooms valid rooms data", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).delete("/dataset/ubc");
+
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("result");
+				expect(res.body.result).to.equal("ubc");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should delete rooms valid sections data", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/sections")
+					.send(sectionsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).delete("/dataset/ubc");
+
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("result");
+				expect(res.body.result).to.equal("ubc");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should have notFoundError", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(roomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).delete("/dataset/ubc2");
+
+				expect(res.status).to.equal(404);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should return 404 notFoundError", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).delete("/dataset/ubc2");
+
+				expect(res.status).to.equal(404);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should return 400 error for invalid id", async function () {
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/ubc/rooms")
+					.send(smallRoomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				const res = await request(SERVER_URL).delete("/dataset/ubc_d");
+
+				expect(res.status).to.equal(400);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+	});
+
+	describe("POST", function () {
+		before(async function () {
+			console.log("Mocha::add dataset");
+			try {
+				await request(SERVER_URL)
+					.put("/dataset/sections/sections")
+					.send(sectionsData)
+					.set("Content-Type", "application/x-zip-compressed");
+
+				await request(SERVER_URL)
+					.put("/dataset/rooms/rooms")
+					.send(roomsData)
+					.set("Content-Type", "application/x-zip-compressed");
+			} catch (err) {
+				expect.fail("ERROR:: thrown when adding dataset");
+			}
+		});
+
+		it("should perform query for sections", async function () {
+			try {
+				const res = await request(SERVER_URL)
+					.post("/query")
+					.send({
+						WHERE: {
+							AND: [
+								{
+									GT: {
+										sections_avg: 74.6,
+									},
+								},
+								{
+									GT: {
+										sections_year: 2011,
+									},
+								},
+								{
+									GT: {
+										sections_audit: 9,
+									},
+								},
+							],
+						},
+						OPTIONS: {
+							COLUMNS: ["sections_audit", "sections_fail", "sections_instructor", "sections_pass"],
+							ORDER: "sections_audit",
+						},
+					})
+					.set("Content-Type", "application/json");
+
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("result");
+				expect(res.body.result).to.have.deep.members([
+					{
+						sections_audit: 10,
+						sections_fail: 0,
+						sections_instructor: "lemay, valerie",
+						sections_pass: 14,
+					},
+					{
+						sections_audit: 11,
+						sections_fail: 1,
+						sections_instructor: "schmidt, mark",
+						sections_pass: 146,
+					},
+					{
+						sections_audit: 18,
+						sections_fail: 0,
+						sections_instructor: "schmidt, mark",
+						sections_pass: 59,
+					},
+					{
+						sections_audit: 19,
+						sections_fail: 0,
+						sections_instructor: "lemay, valerie;tait, david e n",
+						sections_pass: 9,
+					},
+				]);
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should perform query for sections", async function () {
+			try {
+				const res = await request(SERVER_URL)
+					.post("/query")
+					.send({
+						WHERE: {
+							IS: {
+								rooms_name: "BIOL_1503",
+							},
+						},
+						OPTIONS: {
+							COLUMNS: ["rooms_name", "rooms_seats", "rooms_href"],
+						},
+					})
+					.set("Content-Type", "application/json");
+
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property("result");
+				expect(res.body.result).to.have.deep.members([
+					{
+						rooms_name: "BIOL_1503",
+						rooms_seats: 16,
+						rooms_href: "http://students.ubc.ca/campus/discover/buildings-and-classrooms/room/BIOL-1503",
+					},
+				]);
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+		it("should return ResultTooLarge error", async function () {
+			try {
+				const res = await request(SERVER_URL)
+					.post("/query")
+					.send({
+						WHERE: {},
+						OPTIONS: {
+							COLUMNS: ["sections_id"]
+						},
+					})
+					.set("Content-Type", "application/json");
+
+				expect(res.status).to.equal(400);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+
+        it("should return error for invalid query", async function () {
+			try {
+				const res = await request(SERVER_URL)
+					.post("/query")
+					.send({
+						WHERE: {},
+						OPTIONS: {
+							COLUMNS: ["sections_id", "rooms_id"]
+						},
+					})
+					.set("Content-Type", "application/json");
+
+				expect(res.status).to.equal(400);
+				expect(res.body).to.have.property("err");
+			} catch (err: any) {
+				expect.fail(err.message);
+			}
+		});
+	});
 	// The other endpoints work similarly. You should be able to find all instructions at the supertest documentation
 });
