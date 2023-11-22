409e3e5e4bdf328880c2734e5e3f973cd870d6d9
JCurl08 <curl@student.ubc.ca>
2023-11-21 19:49:24 -0800

    fixed tests

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -28,7 +28,7 @@ describe("InsightFacade", async function () {
 	});
 
 	afterEach(function () {
-		// clearDisk();
+		clearDisk();
 	});
 
 	describe("addDataset", function () {
@@ -461,7 +461,7 @@ describe("InsightFacade", async function () {
 		});
 
 		context("when adding many rooms", function () {
-			it.only("should successfully add 364 rooms", async function () {
+			it.("should successfully add 364 rooms", async function () {
 				try {
 					const zip = getContentFromArchives("campus.zip");
 					const add = await facade.addDataset("rooms", zip, InsightDatasetKind.Rooms);
